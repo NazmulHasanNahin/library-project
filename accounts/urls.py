a805b5242f99480bc3c5aeb49f6f2fb24254ad5b
NazmulHasanNahin <nahinn75@gmail.com>
2024-07-13 13:46:02 +0600

    pic adding

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from accounts.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 #this is urls.py of accounts
 urlpatterns = [
     path("register/",register, name="register"),
@@ -9,4 +11,4 @@ urlpatterns = [
     path('deposit/',deposit, name='deposit'),
     path('borrow/<int:book_id>/',borrow_book, name='borrow_book'),
     path('return/<int:history_id>/',return_book, name='return_book'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- book_app/urls.py
+++ book_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('<int:book_id>/', views.book_detail, name='book_detail'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
